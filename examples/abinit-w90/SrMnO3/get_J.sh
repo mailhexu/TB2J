432cb51b3ba0f5db06dc4833303b4615fe7e31eb
hexu <mailhexu@gmail.com>
2020-08-17 22:10:22 +0200

    update example file

--- examples/abinit-w90/SrMnO3/get_J.sh
+++ examples/abinit-w90/SrMnO3/get_J.sh
@@ -1,1 +1,1 @@
-wann2J.py --poscar abinit.in --efermi 6.15 --kmesh 5 5 5 --elements Mn --prefix_up abinito_w90_up --prefix_down abinito_w90_down  --emax 0.0 --height 0.1
+wann2J.py --posfile abinit.in --efermi 6.15 --kmesh 5 5 5 --elements Mn --prefix_up abinito_w90_up --prefix_down abinito_w90_down  --emax 0.0 --height 0.1
