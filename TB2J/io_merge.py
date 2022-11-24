7f7fc570f05385ad4812244f8484a88543274c41
antelmor <at00021@mdg16.wvu.edu>
2022-11-23 22:37:59 -0500

    Improved merging process

--- TB2J/io_merge.py
+++ TB2J/io_merge.py
@@ -1,68 +1,65 @@
 import os
 import copy
 import numpy as np
-from scipy.spatial.transform import Rotation
+from itertools import product
 from TB2J.io_exchange import SpinIO
 
-# Rotation from x to z
-Rxz = Rotation.from_euler('y', -90, degrees=True)
-# Rotation from y to z
-Ryz = Rotation.from_euler('x', -90, degrees=True)
+uy = np.array([0.0, 1.0, 0.0])
+uz = np.array([0.0, 0.0, 1.0])
 
+def orthplane(a):
 
-def rot_merge_DMI(Dx, Dy, Dz):
-    Dx_z = Rxz.apply(Dx)
-    Dy_z = Ryz.apply(Dy)
-    D = (np.array([0.0, 0.5, 0.5]) * Dx_z + np.array([0.5, 0.0, 0.5]) * Dy_z +
-         np.array([0.5, 0.5, 0.0]) * Dz)
-    return D
+    b = np.cross(uy, a)
+    norm_b = np.linalg.norm(b)
+    if norm_b < 1e-10:
+        b = np.cross(uz, a)
+    c = np.cross(a, b)
+    norm_c = np.linalg.norm(c)
 
+    return np.array([
+        b / norm_b,
+        c / norm_c
+    ])
 
-def rot_merge_DMI2(Dx, Dy, Dz):
-    Dx_z = Rxz.apply(Dx)
-    Dy_z = Ryz.apply(Dy)
-    D = (np.array([1, 0, 0]) * Dx_z + np.array([0, 1, 0]) * Dy_z +
-         np.array([0, 0, 1]) * Dz)
-    return D
+def flat_projections(a):
 
+    vectors = []
+    for u, v in product(a, a):
+        vectors.append(
+            np.hstack([u*v, np.roll(u, -1)*v + np.roll(v, -1)*u])
+        )
 
-def merge_DMI(Dx, Dy, Dz):
-    D = (np.array([0.0, 0.5, 0.5]) * Dx + np.array([0.5, 0.0, 0.5]) * Dy +
-         np.array([0.5, 0.5, 0.0]) * Dz)
-    return D
+    return np.vstack(vectors)
 
+class SpinIO_merge(SpinIO):
+    def __init__(self, *args, **kwargs):
+        super(SpinIO_merge, self).__init__(*args, **kwargs)
+        self.projv = None
 
-def merge_DMI2(Dx, Dy, Dz):
-    D = (np.array([1, 0, 0]) * Dx + np.array([0, 1, 0]) * Dy +
-         np.array([0, 0, 1]) * Dz)
-    return D
+    def _set_projection_vectors(self):
 
+        spinat = self.spinat
+        ind_atoms = self.ind_atoms
+        projv = {}
+        for i in range(self.nspin):
+            for j in range(i, self.nspin):
+                a, b = spinat[(ind_atoms[i], ind_atoms[j]), :]
+                v = np.cross(a, b)
+                norm = np.linalg.norm(v)
+                if norm < 1e-6:
+                    projv[i, j] = orthplane(a)
+                else:
+                    projv[i, j] = (v / norm).reshape((1, 3))
+                projv[j, i] = projv[i, j]
 
-def swap_direction(m, idirections):
-    """
-    swap two directions of a tensor m.
-    idirections: the index of the two directions.
-    """
-    idirections = list(idirections)
-    inv = [idirections[1], idirections[0]]
-    n = np.copy(m)
-    n[:, idirections] = n[:, inv]
-    n[idirections, :] = n[inv, :]
-    return n
+        self.projv = projv
 
+    @classmethod
+    def load_pickle(cls, path='TB2J_results', fname='TB2J.pickle'):
+        obj = super(SpinIO_merge, cls).load_pickle(path=path, fname=fname)
+        obj._set_projection_vectors()
 
-def test_swap():
-    m = np.reshape(np.arange(9), (3, 3))
-    print(m)
-    print(swap_direction(m, [0, 1]))
-
-
-def merge_Jani(Janix, Janiy, Janiz):
-    Jani = (np.array([[0, 0, 0], [0, 1, 1], [0, 1, 1]]) * Janix +
-            np.array([[1, 0, 1], [0, 0, 0], [1, 0, 1]]) * Janiy +
-            np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]]) * Janiz) / 2.0
-    return Jani
-
+        return obj
 
 def read_pickle(path):
     p1 = os.path.join(path, 'TB2J_results', 'TB2J.pickle')
@@ -70,130 +67,108 @@ def read_pickle(path):
     if os.path.exists(p1) and os.path.exists(p2):
         print(f" WARNING!: Both file {p1} and {p2} exist. Use default {p1}.")
     if os.path.exists(p1):
-        ret = SpinIO.load_pickle(os.path.join(path, 'TB2J_results'))
+        ret = SpinIO_merge.load_pickle(os.path.join(path, 'TB2J_results'))
     elif os.path.exists(p2):
-        ret = SpinIO.load_pickle(path)
+        ret = SpinIO_merge.load_pickle(path)
     else:
         raise FileNotFoundError(f"Cannot find either file {p1} or {p2}")
     return ret
 
-
 class Merger():
-    def __init__(self, path_x, path_y, path_z, method='structure'):
-        assert (method in ['structure', 'spin'])
-        self.dat_x = read_pickle(path_x)
-        self.dat_y = read_pickle(path_y)
-        self.dat_z = read_pickle(path_z)
-        self.dat = copy.copy(self.dat_z)
-        self.paths = [path_x, path_y, path_z]
+    def __init__(self, *paths, main_path=None, method='structure'):
+        if method not in ['structure', 'spin']:
+            raise ValueError(f"Unrecognized method '{method}'. Available options are: 'structure' or 'spin'.")
         self.method = method
+        self.dat = [read_pickle(path) for path in paths]
+
+        if main_path is None:
+            self.main_dat = copy.deepcopy(self.dat[-1])
+        else:
+            self.main_dat = read_pickle(main_path)
+            self.dat.append(copy.deepcopy(self.main_dat))
+
+        self._set_projv()
+        
+    def _set_projv(self):
+
+        rotated = self.method == 'structure'
+        if rotated:
+            cell = self.main_dat.atoms.cell.array
+            rotated_cells = np.stack(
+                [obj.atoms.cell.array for obj in self.dat], axis=0
+            )
+            R = np.linalg.solve(cell, rotated_cells)
+
+        projv = {}; projv_Jani = {};
+        for key in self.main_dat.projv.keys():
+            vectors = [obj.projv[key] for obj in self.dat]
+            if rotated:
+                vectors = [(R[i] @ vectors[i].T).T for i in range(len(R))]
+            Jani_projections = [flat_projections(v) for v in vectors]
+            projv[key] = np.vstack(vectors)
+            projv_Jani[key] = np.vstack(Jani_projections)
+        
+        self.projv = projv
+        self.projv_Jani = projv_Jani
 
     def merge_Jani(self):
         Jani_dict = {}
-        Janixdict = self.dat_x.Jani_dict
-        Janiydict = self.dat_y.Jani_dict
-        Janizdict = self.dat_z.Jani_dict
-        for key, Janiz in Janizdict.items():
+        projv_Jani = self.projv_Jani
+        for key in self.main_dat.Jani_dict.keys():
             try:
                 R, i, j = key
-                keyx = R
-                keyy = R
-                Janix = Janixdict[(tuple(keyx), i, j)]
-                Janiy = Janiydict[(tuple(keyy), i, j)]
+                proj_list = []
+                for obj in self.dat:
+                    v = obj.projv[i, j]
+                    proj_list.append((v @ obj.Jani_dict[key] @ v.T).flatten())
+                projections = np.hstack(proj_list)
             except KeyError as err:
                 raise KeyError(
                     "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                     % err)
-            if self.method == 'spin':
-                Jani_dict[key] = merge_Jani(Janix, Janiy, Janiz)
-            else:
-                Jani_dict[key] = merge_Jani(swap_direction(Janix, (0, 2)),
-                                            swap_direction(Janiy, (1, 2)),
-                                            Janiz)
-        self.dat.Jani_dict = Jani_dict
+            Jani = np.linalg.lstsq(projv_Jani[i, j], projections, rcond=None)[0]
+            Jani_dict[key] = np.array([
+                [Jani[0], Jani[3], Jani[5]],
+                [Jani[3], Jani[1], Jani[4]],
+                [Jani[5], Jani[4], Jani[2]]
+            ])
+        self.main_dat.Jani_dict = Jani_dict
 
     def merge_Jiso(self):
         Jdict={}
-        Jxdict=self.dat_x.exchange_Jdict
-        Jydict=self.dat_y.exchange_Jdict
-        Jzdict=self.dat_z.exchange_Jdict
-        for key, J in Jzdict.items():
+        for key in self.main_dat.exchange_Jdict.keys():
             try:
-               Jx = Jxdict[key]
-               Jy = Jydict[key]
-               Jz = Jzdict[key]
+               J = np.mean([obj.exchange_Jdict[key] for obj in self.dat])
             except KeyError as err:
                 raise KeyError(
                         "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                         % err)
-            Jdict[key]=(Jx+Jy+Jz)/3.0
-        self.dat.exchange_Jdict=Jdict
+            Jdict[key] = J
+        self.main_dat.exchange_Jdict = Jdict
                
 
     def merge_DMI(self):
         dmi_ddict = {}
-        if self.dat_x.has_dmi and self.dat_y.has_dmi and self.dat_z.has_dmi:
-            Dxdict = self.dat_x.dmi_ddict
-            Dydict = self.dat_y.dmi_ddict
-            Dzdict = self.dat_z.dmi_ddict
-            for key, Dz in Dzdict.items():
+        if all(obj.has_dmi for obj in self.dat):
+            projv = self.projv
+            for key in self.main_dat.dmi_ddict.keys():
                 try:
                     R, i, j = key
-                    keyx = R
-                    keyy = R
-                    Dx = Dxdict[(tuple(keyx), i, j)]
-                    Dy = Dydict[(tuple(keyy), i, j)]
+                    proj_list = [obj.projv[i, j] @ obj.dmi_ddict[key] for obj in self.dat]
+                    projections = np.hstack(proj_list)
                 except KeyError as err:
                     raise KeyError(
                         "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                         % err)
-                if self.method == 'structure':
-                    dmi_ddict[key] = rot_merge_DMI(Dx, Dy, Dz)
-                else:
-                    dmi_ddict[key] = merge_DMI(Dx, Dy, Dz)
-            self.dat.dmi_ddict = dmi_ddict
+                DMI = np.linalg.lstsq(projv[i, j], projections, rcond=None)[0]
+                dmi_ddict[key] = DMI
+            self.main_dat.dmi_ddict = dmi_ddict
 
-        dmi_ddict = {}
-        try:
-            Dxdict = self.dat_x.debug_dict['DMI2']
-            Dydict = self.dat_y.debug_dict['DMI2']
-            Dzdict = self.dat_z.debug_dict['DMI2']
-            for key, Dz in Dzdict.items():
-                try:
-                    R, i, j = key
-                    keyx = R
-                    keyy = R
-                    Dx = Dxdict[(tuple(keyx), i, j)]
-                    Dy = Dydict[(tuple(keyy), i, j)]
-                except KeyError as err:
-                    raise KeyError(
-                        "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
-                        % err)
-                if self.method == 'structure':
-                    dmi_ddict[key] = rot_merge_DMI2(Dx, Dy, Dz)
-                elif self.method == 'spin':
-                    dmi_ddict[key] = merge_DMI2(Dx, Dy, Dz)
-
-            self.dat.debug_dict['DMI2'] = dmi_ddict
-        except:
-            pass
-
-    def write(self, path='TB2J_results'):
-        self.dat.description += 'Merged from TB2J results in paths: \n  ' + '\n  '.join(
-            self.paths) + '\n'
-        if self.method == 'spin':
-            self.dat.description += ', which are from DFT data with spin along x, y, z orientation\n'
-        elif self.method == 'structure':
-            self.dat.description += ', which are from DFT data with structure with z axis rotated to x, y, z\n'
-        self.dat.description += '\n'
-        self.dat.write_all(path=path)
-
-
-def merge(path_x, path_y, path_z, method, save=True, path='TB2J_results'):
-    m = Merger(path_x, path_y, path_z, method)
+def merge(*paths, main_path=None, method='structure', save=True, write_path='TB2J_results'):
+    m = Merger(*paths, main_path=main_path, method=method)
     m.merge_Jiso()
     m.merge_DMI()
     m.merge_Jani()
     if save:
-        m.write(path=path)
+        m.main_dat.write_all(path=write_path)
     return m.dat
--- scripts/TB2J_merge.py
+++ scripts/TB2J_merge.py
@@ -8,24 +8,31 @@ from TB2J.io_merge import merge
 def main():
     parser = argparse.ArgumentParser(
         description=
-        "TB2J_merge: merge the exchange parameters calculated from different directions. It can accept two types of calculations, first, the lattice structures are rotated from the original structure, from z to x, y, and z, respectively. The structures could be generated using the TB2J_rotate.py command. The second type is the three calculations have the spins along the x, y, and z axis, respectively."
+        "TB2J_merge: merge the exchange parameters calculated from different directions. It can accept two types of calculations, first, the lattice structures are rotated from the original structure. The structures could be generated using the TB2J_rotate.py command. The second type consists of different calculations with different spin orientations."
     )
     parser.add_argument(
         'directories',
         type=str,
         nargs='+',
         help=
-        'The three directories the TB2J calculations are done. Inside each of them, there should be a TB2J_results directory which contains the magnetic interaction parameter files. e.g. Fe_x Fe_y Fe_z. Alternatively, it could  be the TB2J results directories.'
+        'The directories where the TB2J calculations are done. Inside each of them, there should be a TB2J_results directory which contains the magnetic interaction parameter files. e.g. Fe_x Fe_y Fe_z. Alternatively, it could  be the TB2J results directories.'
+    )
+    parser.add_argument(
+        'main_directory',
+        type=str,
+        default=None,
+        help=
+        'The directory containning the TB2J calculation of the reference structure. If not given the last element of the "directories" argumetn will be used'
     )
     parser.add_argument(
         '--type',
         '-T',
         type=str,
+        default='structure',
         help=
         'The type of calculations, either structure of spin, meaning that the three calculations are done by rotating the structure/spin. '
     )
     args = parser.parse_args()
-    merge(*(args.directories), args.type.strip().lower())
-
+    merge(*(args.directories), main_path=args.main_directory, method=args.type.strip().lower(), )
 
 main()
