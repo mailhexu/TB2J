df90e9816d21a6071bbc2330dd9cf452ebcd5cda
Xu He <mailhexu@gmail.com>
2025-04-29 07:58:42 +0200

    add parameter --index_magnetic_atoms

--- scripts/abacus2J.py
+++ scripts/abacus2J.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 
+from TB2J.exchange_params import add_exchange_args_to_parser
 from TB2J.interfaces import gen_exchange_abacus
 from TB2J.versioninfo import print_license
 
@@ -11,11 +12,10 @@ def run_abacus2J():
     parser = argparse.ArgumentParser(
         description="abacus2J: Using magnetic force theorem to calculate exchange parameter J from abacus Hamiltonian in the LCAO mode"
     )
-
+    # Add ABACUS specific arguments
     parser.add_argument(
         "--path", help="the path of the abacus calculation", default="./", type=str
     )
-
     parser.add_argument(
         "--suffix",
         help="the label of the abacus calculation. There should be an output directory called OUT.suffix",
@@ -23,98 +23,8 @@ def run_abacus2J():
         type=str,
     )
 
-    parser.add_argument(
-        "--elements",
-        help="list of elements to be considered in Heisenberg model.",
-        # ,  For each element, a postfixes can be used to specify the orbitals(Only with Siesta backend), eg. Fe_3d, or Fe_3d_4s ",
-        default=None,
-        type=str,
-        nargs="*",
-    )
-    parser.add_argument(
-        "--rcut",
-        help="range of R. The default is all the commesurate R to the kmesh",
-        default=None,
-        type=float,
-    )
-    parser.add_argument(
-        "--efermi", help="Fermi energy in eV. For test only. ", default=None, type=float
-    )
-    parser.add_argument(
-        "--kmesh",
-        help="kmesh in the format of kx ky kz. Monkhorst pack. If all the numbers are odd, it is Gamma cenetered. (strongly recommended), Default: 5 5 5",
-        type=int,
-        nargs="*",
-        default=[5, 5, 5],
-    )
-    parser.add_argument(
-        "--emin",
-        help="energy minimum below efermi, default -14 eV",
-        type=float,
-        default=-14.0,
-    )
-
-    parser.add_argument(
-        "--use_cache",
-        help="whether to use disk file for temporary storing wavefunctions and hamiltonian to reduce memory usage. Default: False",
-        action="store_true",
-        default=False,
-    )
-
-    parser.add_argument(
-        "--nz", help="number of integration steps. Default: 50", default=50, type=int
-    )
-
-    parser.add_argument(
-        "--cutoff",
-        help="The minimum of J amplitude to write, (in eV). Default: 1e-7 eV",
-        default=1e-7,
-        type=float,
-    )
-
-    parser.add_argument(
-        "--exclude_orbs",
-        help="the indices of wannier functions to be excluded from magnetic site. counting start from 0. Default is none.",
-        default=[],
-        type=int,
-        nargs="+",
-    )
-
-    parser.add_argument(
-        "--nproc",
-        "--np",
-        help="number of cpu cores to use in parallel, default: 1",
-        default=1,
-        type=int,
-    )
-
-    parser.add_argument(
-        "--description",
-        help="add description of the calculatiion to the xml file. Essential information, like the xc functional, U values, magnetic state should be given.",
-        type=str,
-        default="Calculated with TB2J.",
-    )
-
-    parser.add_argument(
-        "--orb_decomposition",
-        default=False,
-        action="store_true",
-        help="whether to do orbital decomposition in the non-collinear mode. Default: False.",
-    )
-
-    parser.add_argument(
-        "--fname",
-        default="exchange.xml",
-        type=str,
-        help="exchange xml file name. default: exchange.xml",
-    )
-
-    parser.add_argument(
-        "--output_path",
-        help="The path of the output directory, default is TB2J_results",
-        type=str,
-        default="TB2J_results",
-    )
+    # Add common exchange arguments
+    parser = add_exchange_args_to_parser(parser)
 
     args = parser.parse_args()
 
@@ -139,6 +49,8 @@ def run_abacus2J():
         nproc=args.nproc,
         exclude_orbs=args.exclude_orbs,
         orb_decomposition=args.orb_decomposition,
+        index_magnetic_atoms=args.index_magnetic_atoms,
+        cutoff=args.cutoff,
     )
 
 
