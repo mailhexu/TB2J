0b49607edce826be971f16e3fcc61e9184554b15
mailhexu <mailhexu@gmail.com>
2025-10-16 14:16:43 +0200

    update Oiju_epw2.py to allow spin up/down epw matrix

--- TB2J/Oiju_epw2.py
+++ TB2J/Oiju_epw2.py
@@ -8,43 +8,42 @@ spin and lattice degrees of freedom.
 """
 
 import os
-import numpy as np
-import copy
-from TB2J.myTB import MyTB, merge_tbmodels_spin
-from TB2J.exchange import ExchangeCL, ExchangeNCL
-from TB2J.exchange_pert2 import ExchangePert2
-from TB2J.utils import read_basis, auto_assign_basis_name
+from os.path import expanduser
+
 from ase.io import read
-from TB2J.FDTB import dHdx
-from TB2J.epw import epw_to_dHdx, generate_TB_with_distortion
-from TB2J.epwparser import Epmat, EpmatOneMode
-from ase.io import write
-from  os.path import expanduser
+
+from TB2J.epwparser import Epmat
+from TB2J.exchange_pert2 import ExchangePert2
+from TB2J.myTB import MyTB, merge_tbmodels_spin
+from TB2J.utils import auto_assign_basis_name
 
 
-def gen_exchange_Oiju_epw(path: str,
-                          colinear=True,
-                          posfile='POSCAR',
-                          prefix_up='wannier90.up',
-                          prefix_dn='wannier90.dn',
-                          prefix_SOC='wannier90',
-                          epw_path='./',
-                          epw_prefix='SrMnO3',
-                          idisp=0,
-                          Ru=(0, 0, 0),
-                          min_hopping_norm=1e-6,
-                          Rcut=None,
-                          efermi=3.,
-                          magnetic_elements=[],
-                          kmesh=[5, 5, 5],
-                          emin=-12.0,
-                          emax=0.0,
-                          nz=50,
-                          np=1,
-                          exclude_orbs=[],
-                          description: str = '',
-                          list_iatom: list = None,
-                          output_path: str = 'TB2J_results'):
+def gen_exchange_Oiju_epw(
+    path: str,
+    colinear=True,
+    posfile="POSCAR",
+    prefix_up="wannier90.up",
+    prefix_dn="wannier90.dn",
+    prefix_SOC="wannier90",
+    epw_path="./",
+    epw_prefix_up="SrMnO3_up",
+    epw_prefix_dn="SrMnO3_dn",
+    idisp=0,
+    Ru=(0, 0, 0),
+    min_hopping_norm=1e-6,
+    Rcut=None,
+    efermi=3.0,
+    magnetic_elements=[],
+    kmesh=[5, 5, 5],
+    emin=-12.0,
+    emax=0.0,
+    nz=50,
+    np=1,
+    exclude_orbs=[],
+    description: str = "",
+    list_iatom: list = None,
+    output_path: str = "TB2J_results",
+):
     """Calculate spin-phonon coupling parameters using EPW data.
 
     This function combines Wannier90 electronic structure with EPW electron-phonon
@@ -67,8 +66,10 @@ def gen_exchange_Oiju_epw(path: str,
         Prefix for SOC Wannier90 files
     epw_path : str, default='./'
         Path to the EPW calculation directory
-    epw_prefix : str, default='SrMnO3'
-        Prefix for EPW files
+    epw_prefix_up : str, default='SrMnO3_up'
+        Prefix for spin-up EPW files
+    epw_prefix_dn : str, default='SrMnO3_dn'
+        Prefix for spin-down EPW files
     idisp : int, default=0
         Index of the atomic displacement pattern to analyze
     Ru : tuple, default=(0,0,0)
@@ -107,43 +108,43 @@ def gen_exchange_Oiju_epw(path: str,
     """
     atoms = read(os.path.join(path, posfile))
     if colinear:
-        tbmodel_up = MyTB.read_from_wannier_dir(path=path,
-                                                prefix=prefix_up,
-                                                atoms=atoms,
-                                                nls=False)
-        tbmodel_dn = MyTB.read_from_wannier_dir(path=path,
-                                                prefix=prefix_dn,
-                                                atoms=atoms,
-                                                nls=False)
+        tbmodel_up = MyTB.read_from_wannier_dir(
+            path=path, prefix=prefix_up, atoms=atoms, nls=False
+        )
+        tbmodel_dn = MyTB.read_from_wannier_dir(
+            path=path, prefix=prefix_dn, atoms=atoms, nls=False
+        )
         tbmodel = merge_tbmodels_spin(tbmodel_up, tbmodel_dn)
 
         # Load spin-resolved EPW data
         epw_up = Epmat()
-        epw_up.read(path=epw_path, prefix=epw_prefix + "_up", epmat_ncfile='epmat.nc')
+        epw_up.read(path=epw_path, prefix=epw_prefix_up, epmat_ncfile="epmat.nc")
         epw_dn = Epmat()
-        epw_dn.read(path=epw_path, prefix=epw_prefix + "_dn", epmat_ncfile='epmat.nc')
+        epw_dn.read(path=epw_path, prefix=epw_prefix_dn, epmat_ncfile="epmat.nc")
 
         basis, _ = auto_assign_basis_name(tbmodel.xred, atoms)
 
-        exchange = ExchangePert2(tbmodels=tbmodel,
-                                 atoms=atoms,
-                                 basis=basis,
-                                 efermi=efermi,
-                                 magnetic_elements=magnetic_elements,
-                                 kmesh=kmesh,
-                                 emin=emin,
-                                 emax=emax,
-                                 nz=nz,
-                                 np=np,
-                                 Rcut=Rcut,
-                                 exclude_orbs=exclude_orbs,
-                                 list_iatom=list_iatom,
-                                 description=description)
+        exchange = ExchangePert2(
+            tbmodels=tbmodel,
+            atoms=atoms,
+            basis=basis,
+            efermi=efermi,
+            magnetic_elements=magnetic_elements,
+            kmesh=kmesh,
+            emin=emin,
+            emax=emax,
+            nz=nz,
+            np=np,
+            Rcut=Rcut,
+            exclude_orbs=exclude_orbs,
+            list_iatom=list_iatom,
+            description=description,
+        )
         exchange.set_epw(Ru, imode=idisp, epmat_up=epw_up, epmat_dn=epw_dn)
         exchange.run(output_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # for imode in range(15):
     # for imode in range(3, 15):
     for idisp in [3, 6, 7]:
@@ -152,25 +153,27 @@ if __name__ == '__main__':
             # path="/home/hexu/projects/SrMnO3/origin_wannier/SrMnO3_FM/center_SrMnO3_slater0.00",
             path=expanduser("~/projects/TB2J_examples/Wannier/SrMnO3_QE_Wannier90/W90"),
             colinear=True,
-            posfile='SrMnO3.scf.pwi',
+            posfile="SrMnO3.scf.pwi",
             # prefix_up='wannier90.up',
             # prefix_dn='wannier90.dn',
             prefix_up="SrMnO3_up",
             prefix_dn="SrMnO3_down",
-            prefix_SOC='wannier90',
-            epw_path=expanduser('~/projects/projects/SrMnO3/epw'),
-            epw_prefix='SrMnO3',
+            prefix_SOC="wannier90",
+            epw_path=expanduser("~/projects/projects/SrMnO3/epw"),
+            epw_prefix_up="SrMnO3_up",
+            epw_prefix_dn="SrMnO3_dn",
             idisp=idisp,
             Ru=(0, 0, 0),
             Rcut=8,
             efermi=10.67,
-            magnetic_elements=['Mn'],
+            magnetic_elements=["Mn"],
             kmesh=[5, 5, 5],
             emin=-7.3363330034071295,
             emax=0.0,
             nz=70,
             np=1,
             exclude_orbs=[],
-            description='',
+            description="",
             # list_iatom=[1],
-            output_path=f"VT_{idisp}_withcutoff0.1")
+            output_path=f"VT_{idisp}_withcutoff0.1",
+        )
